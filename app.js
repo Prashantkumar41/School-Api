58f57cb601aeeeae8aef0ae417ad92432fcea138
Prashant kumar <prashantkumarsw91@gmail.com>
2025-05-23 18:24:43 +0530

    changes perform in app.js

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ app.use(express.json());
 app.use('/api', schoolRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
