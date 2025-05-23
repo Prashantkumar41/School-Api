234c00716e1f86a577f4364929138cc8431a341b
Prashant kumar <prashantkumarsw91@gmail.com>
2025-05-23 18:50:08 +0530

    changes on database

--- config/db.js
+++ config/db.js
@@ -4,10 +4,12 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT, // ✅ include this!
+
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-const promisePool = pool.promise(); // ✅ This enables use of .execute()
+const promisePool = pool.promise(); 
 
 module.exports = promisePool;
